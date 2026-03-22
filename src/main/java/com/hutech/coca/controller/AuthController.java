0290f877d0b068463ef9625575356af8401a1d10
PHAN DINH TUAN <pdtuan04@gmail.com>
2026-03-22 20:59:57 +0700

    add reg log by google

--- src/main/java/com/hutech/coca/controller/AuthController.java
+++ src/main/java/com/hutech/coca/controller/AuthController.java
@@ -1,15 +1,26 @@
 package com.hutech.coca.controller;
 
+import com.hutech.coca.dto.GoogleLoginRequest;
+import com.hutech.coca.model.Role;
 import com.hutech.coca.model.User;
+import com.hutech.coca.service.AuthService;
 import com.hutech.coca.service.UserService;
 import com.hutech.coca.utils.JwtUtils;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -18,8 +29,9 @@ public class AuthController {
 
     private final UserService userService;
     private final JwtUtils jwtUtils;
+    private final AuthService authService;
     private final PasswordEncoder passwordEncoder;
-
+    private String googleClientId;
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody User user) {
@@ -56,5 +68,25 @@ public class AuthController {
                     ));
                 })
                 .orElse(ResponseEntity.status(401).body(Map.of("error", "Sai tài khoản hoặc mật khẩu")));
+    }
+    @PostMapping("/google")
+    public ResponseEntity<Map<String, Object>> authenticateGoogleUser(@RequestBody GoogleLoginRequest request) {
+        try {
+            Map<String, Object> responseData = authService.authenticateWithGoogle(request.getToken());
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", true);
+            response.put("message", "Đăng nhập Google thành công");
+            response.put("data", responseData);
+
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("success", false);
+            errorResponse.put("message", "Lỗi server: " + e.getMessage());
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
     }
 }
